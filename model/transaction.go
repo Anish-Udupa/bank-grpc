a81df60e82ddc2250637f80db7fe92945bfb37af
Anish Udupa H <75924439+Anish-Udupa@users.noreply.github.com>
2024-09-14 11:42:26 +0530

    Addition of TrasnactionType to fix DEPOSIT/WITHDRAW msg issue (#2)

--- model/transaction.go
+++ model/transaction.go
@@ -1,11 +1,16 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"bank-grpc/enums"
+
+	"gorm.io/gorm"
+)
 
 type Transaction struct {
 	gorm.Model
-	FromAccountNumber uint32 `json:"from_account_number"`
-	ToAccountNumber   uint32
-	Amount            uint32
-	Message           string
+	FromAccountNumber uint32                `json:"from_account_number"`
+	ToAccountNumber   uint32                `json:"to_account_number"`
+	TransactionType   enums.TransactionType `json:"transaction_type"`
+	Amount            uint32                `json:"amount"`
+	Message           string                `json:"message"`
 }
